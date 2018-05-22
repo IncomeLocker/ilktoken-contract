e33a5d7d356c131249e2a71997038df3fed5d940
iFA <ifa@fusionsolutions.io>
2018-05-22 17:36:11 +0200

    Create token_db.sol

---
+++ contract/token_db.sol
@@ -0,0 +1,56 @@
+/*
+    Token database
+    token_db.sol
+    1.0.0
+*/
+pragma solidity 0.4.24;
+
+import "./safeMath.sol";
+import "./owned.sol";
+
+contract TokenDB is Owned {
+    /* Declarations */
+    using SafeMath for uint256;
+    /* Structures */
+    // struct vesting_s {
+    //     address giver;
+    //     uint256 amount;
+    //     uint256 start;
+    //     uint256 end;
+    //     uint256 received;
+    // }
+    /* Variables */
+    mapping(address => mapping(address => uint256)) public allowance;
+    mapping(address => uint256) public balances;
+    // mapping(address => vesting_s[]) public vestings;
+    address public tokenAddress;
+    /* Constructor */
+    /* Externals */
+    function changeTokenAddress(address _tokenAddress) external forOwner {
+        tokenAddress = _tokenAddress;
+    }
+    function transfer(address _from, address _to, uint256 _amount) external forToken returns(bool success) {
+        balances[_from] = balances[_from].sub(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        return true;
+    }
+    function setAllowance(address _owner, address _spender, uint256 _amount) external forToken returns(bool success) {
+        allowance[_owner][_spender] = _amount;
+        return true;
+    }
+    /* Constants */
+    function getAllowance(address _owner, address _spender) public constant returns(bool success, uint256 remaining) {
+        return ( true, allowance[_owner][_spender] );
+    }
+    function balanceOf(address _owner) public constant returns(bool success, uint256 balance) {
+        return ( true, balances[_owner]);
+    }
+    /* Modifiers */
+    modifier forToken {
+        if ( tokenAddress == 0x00 ) {
+            tokenAddress = msg.sender;
+        }
+        require( msg.sender == tokenAddress );
+        _;
+    }
+}
