e2ef67ded9544438c30962cdf3cffc2070a04030
iFA <ifa@fusionsolutions.io>
2018-05-22 15:46:04 +0200

    Update owned.sol

--- contract/owned.sol
+++ contract/owned.sol
@@ -1,7 +1,7 @@
 /*
-    Contract owner
+    Owner
     owned.sol
-    1.0.0
+    1.0.1
 */
 pragma solidity 0.4.24;
 
@@ -10,10 +10,13 @@ contract Owned {
     address public owner = msg.sender;
     /* Constructor */
     constructor(address _owner) public {
+        if ( _owner == 0x00 ) {
+            _owner = msg.sender;
+        }
         owner = _owner;
     }
     /* Externals */
-    function replaceOwner(address _owner) external returns(bool success) {
+    function replaceOwner(address _owner) external returns(bool) {
         require( isOwner() );
         owner = _owner;
         return true;
